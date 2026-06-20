7bda74b3d22de8e34647dfe4ab314f0940aeb753
FSDPrathamesh <prathameshdalvi036@gmail.com>
2026-06-20 11:37:53 +0530

    button annimation and timeline update

--- logic/universal.js
+++ logic/universal.js
@@ -13,6 +13,20 @@ buttons.forEach((button) => {
   });
 });
 
+const glowButtons = document.querySelectorAll(".button-colour");
+
+glowButtons.forEach((button) => {
+    button.addEventListener("mousemove", (e) => {
+        const rect = button.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        button.style.setProperty("--x", `${x}px`);
+        button.style.setProperty("--y", `${y}px`);
+    });
+});
+
 
 // Counter Js universal 
 document.addEventListener("DOMContentLoaded", () => {
