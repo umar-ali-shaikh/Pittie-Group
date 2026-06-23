6e02757154f51e4a2fbc547364ef0bb5f30f6307
FSDPrathamesh <prathameshdalvi036@gmail.com>
2026-06-23 13:08:29 +0530

    Video changed of bodify and yomkist para change

--- logic/innovation.js
+++ logic/innovation.js
@@ -20,3 +20,18 @@ const observer = new IntersectionObserver(
 
 observer.observe(slide);
 
+
+
+document.querySelectorAll(".frame-shining").forEach((card) => {
+  const glow = card.querySelector(".shine-dot");
+
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    glow.style.left = `${x}px`;
+    glow.style.top = `${y}px`;
+  });
+});
